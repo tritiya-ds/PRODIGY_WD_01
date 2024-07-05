d9350843546b088f102cb180915db4eb9406009a
Tritiya Dey Sarkar <168653192+tritiya-ds@users.noreply.github.com>
2024-07-05 12:14:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//   /* .card{
+//    width: 200px;
+//    height: 300px;
+//    border-radius: 20px;
+//    /* overflow: hidden; */
+//    border: 8px solid white;
+//    position: relative;
+//   }
+//   .card-img{
+//    width: 100%;
+//    height: 100%;
+//    object-fit: cover;
+//    border-radius: 15px;
+//   }
+//   .card-body{
+//    height: 100%;
+//    width: 100%;
+//    top: 0;
+//    right: 0;
+//    position: absolute;
+//    background: #ffae0150;
+//    backdrop-filter: blur(5px);
+//    border-radius: 15px;
+//    color:black;
+//    padding: 30px;
+
+//   }
+//   .card-heading{
+//    text-transform: uppercase;
+//    font-size: 25px;
+//    font-weight: 500;
+
+//   }
+//   .card-brief{
+//    text-transform: capitalize;
+//    font-size: 14px;
+//    font-weight: 300;
+//   } */
